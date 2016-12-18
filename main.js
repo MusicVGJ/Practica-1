06e52677f1dc550ac28707ea52dea0b1bc935ca1
GerardU <gerardubasart07@gmail.com>
2016-12-18 21:04:34 +0100

    Prova

--- main.js
+++ main.js
@@ -1,25 +1,6 @@
 /**
  * Created by Victoria on 15/12/16.
  */
-$(function () {
-    // Grab the template script
-    var theTemplateScript = $("#expressions-template").html();
-
-    // Compile the template
-    var theTemplate = Handlebars.compile(theTemplateScript);
-
-    // Define our data object
-    var context={
-        "description": {
-            "escaped": "Using {{}} brackets will result in escaped HTML:",
-            "unescaped": "Using {{{}}} will leave the context as it is:"
-        },
-        "example": "<button> Hello </button>"
-    };
-
-    // Pass our data to the template
-    var theCompiledHtml = theTemplate(context);
-
-    // Add the compiled html to the page
-    $('.content-placeholder').html(theCompiledHtml);
+(function () {
+        console.log("Hola Caracola");
 });
