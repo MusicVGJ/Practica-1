96a673f02fb963cb62789b0642077f514a79d742
Victoria Vergaray <maria.victoria8796@gmail.com>
2016-12-20 23:36:45 +0100

    ClasseDijous20
    Hem fet la capçalera de la web

--- mainJS.js
+++ mainJS.js
@@ -8,24 +8,27 @@
 
 function crearCabecera(){
 
-    var sectionHeader = document.createElement("section");
     var header = document.createElement("header");
+    var container1 = document.createElement("container");
 
     header.id = "cabecera";
     header.className = "header-full-fixed";
+    container1.id="cont1";
 
     var logo = document.createElement("a");
     var logo_img = document.createElement("img");
 
     logo_img.className = "header-logo";
-    logo_img.src = "https://s20.postimg.org/7imyozesd/lsoundfinal.png";
+    logo_img.id="LogoID";
+    logo_img.src = "https://s20.postimg.org/9e9r053ml/lsoundfinal.png";
     logo_img.alt = "Logo";
 
     logo.href = "";
 
     logo.appendChild(logo_img);
+    container1.appendChild(logo);
+    header.appendChild(container1);
     header.appendChild(logo);
-    sectionHeader.appendChild(header);
-    document.body.appendChild(sectionHeader);
+    document.body.appendChild(header);
 
 }
