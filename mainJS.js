52bf35cc0e2d937e8dd7a6e30045add35a0b6221
GerardU <gerardubasart07@gmail.com>
2017-01-03 19:03:55 +0100

    Barra Menú i Seccions
    Continuem amb la utilització de la llibreria Bootstrap

--- mainJS.js
+++ mainJS.js
@@ -1,5 +1,6 @@
 (function (){
 
+
     crearCabecera();
 
 
@@ -7,13 +8,15 @@
 }());
 
 function crearCabecera(){
-
+/*
     var header = document.createElement("header");
     var container1 = document.createElement("container");
 
     header.id = "cabecera";
     header.className = "header-full-fixed";
     container1.id="cont1";
+    container1.className= "container-fluid";
+    container1.style.backgroundColor = "black";
 
     var logo = document.createElement("a");
     var logo_img = document.createElement("img");
@@ -28,8 +31,160 @@ function crearCabecera(){
     logo.appendChild(logo_img);
     container1.appendChild(logo);
     header.appendChild(container1);
-    header.appendChild(logo);
+
     document.body.appendChild(header);
+*/
+
+    var header = document.createElement("header");
+    var nav = document.createElement("nav");
+    var div = document.createElement("div");
+    var barra = document.createElement("div");
+
+
+    div.className = "container-fluid";
+    div.style.backgroundColor = "black";
+    nav.className="navbar navbar-inverse navbar-fixed-top";
+
+    header.appendChild(nav);
+    nav.appendChild(div);
+
+    barra.className = "navbar-header";
+
+    var menuButton = document.createElement("button");
+
+    menuButton.type = "button";
+    menuButton.className = "navbar-toggle";
+    menuButton.setAttribute('data-toggle','collapse');
+    menuButton.setAttribute('data-target','#myNavbar');
+
+    for(i = 0; i<3;i++){
+
+        var span = document.createElement("span");
+        span.className = "icon-bar";
+        menuButton.appendChild(span);
+
+    }
+
+    barra.appendChild(menuButton);
+
+    var a = document.createElement("a");
+    a.className = "navbar-Brand"
+    a.href = "#";
+    var logo = document.createElement("img");
+    logo.src = "https://s20.postimg.org/nwechnqfx/logo.gif";
+    logo.className = "img-responsive";
+    logo.style.height = "50px";
+    logo.style.float = "left";
+
+
+    a.appendChild(logo);
+
+    var seccions = document.createElement("div");
+
+    seccions.className = "collapse navbar-collapse";
+    seccions.id = "myNavbar";
+
+    var ul = document.createElement("ul");
+    ul.className = "nav navbar-nav";
+
+    var li1 = document.createElement("li");
+    var li2 = document.createElement("li");
+    var li3 = document.createElement("li");
+
+    var a1 = document.createElement("a");
+    var a2 = document.createElement("a");
+
+    var t1 = document.createTextNode("Recomanats");
+    var t2 = document.createTextNode("Top");
+
+    var aux = document.createElement("div");
+    var form = document.createElement("form");
+    var input = document.createElement("input");
+    var bBuscador = document.createElement("button");
+    var i = document.createElement("i");
+    var aux2 = document.createElement("div");
+
+    bBuscador.type = "submit";
+    bBuscador.method = "POST";
+    bBuscador.className = "btn btn-default";
+    i.className = "glyphicon glyphicon-search";
+    form.className="navbar-form";
+    form.setAttribute('role','search');
+    aux.className = "input-group";
+    aux2.className = "input-group-btn";
+
+    input.type =  "text";
+    input.className = "form-control";
+    input.placeholder = "Busca la teva música";
+
+    bBuscador.appendChild(i);
+
+    //input.style.float = "left";
+    //bBuscador.style.float = "left";
+
+    aux.appendChild(input);
+    aux2.appendChild(bBuscador);
+    aux.appendChild(aux2);
+    form.appendChild(aux);
+
+
+    a1.href = "#recomenats";
+    a1.appendChild(t1);
+
+    a2.href = "#top";
+    a2.appendChild(t2);
+
+    li1.appendChild(a1);
+    li2.appendChild(a2);
+    li3.appendChild(aux);
+
+    ul.appendChild(li1);
+    ul.appendChild(li2);
+    ul.appendChild(li3);
+
+    seccions.appendChild(ul);
+    barra.appendChild(a);
+
+    var sRecomenats = document.createElement("section");
+    var sBuscador = document.createElement("section");
+    var sTop = document.createElement("section");
+
+    sRecomenats.id = "recomenats";
+    sRecomenats.className = "container-fluid";
+
+    sBuscador.id = "section2";
+    sBuscador.className = "container-fluid";
+
+    sTop.id = "top";
+    sTop.className = "container-fluid";
+
+    var hRecomenats = document.createElement("h1");
+    var title1 = document.createTextNode("Recomentas per a tu");
+    hRecomenats.appendChild(title1);
+
+    var hTop = document.createElement("h1");
+    var title2 = document.createTextNode("Top del moment");
+    hTop.appendChild(title2);
+
+    var hBuscador = document.createElement("h1");
+    var title3 = document.createTextNode("Resultats de la búsqueda");
+    hBuscador.appendChild(title3);
+
+
+    sRecomenats.appendChild(hRecomenats);
+    sTop.appendChild(hTop);
+    sBuscador.appendChild(hBuscador);
+
+
+    div.appendChild(barra);
+    div.appendChild(seccions);
+    document.body.appendChild(header);
+
+    document.body.appendChild(sRecomenats);
+    document.body.appendChild(sTop);
+    document.body.appendChild(sBuscador);
+
+
 
 }
 
